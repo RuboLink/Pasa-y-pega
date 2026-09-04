8117162385883535d121209f587fa322e15bb157
Rubén Martínez Sánchez <ruboms@gmail.com>
2026-09-04 19:38:49 +0200

    update

--- Enemigo.java
+++ Enemigo.java
@@ -1,11 +1,15 @@
 public interface Enemigo {
     public static int vida = 100;
     public static int defensa = 100;
+    public static String nombre = "Pepito";
 
     void setVida();
     int getVida();
 
     void setDefensa();
     int getDefensa();
+
+    void setNombre();
+    String getNombre();
 
 }
